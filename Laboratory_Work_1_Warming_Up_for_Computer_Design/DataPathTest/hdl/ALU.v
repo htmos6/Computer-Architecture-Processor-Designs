f438d871654a9f85e5facd1f1a998cb59d7e56fd
htmos6 <mehmet.arslan4000@gmail.com>
2023-04-11 18:56:04 +0300

    experiment 1 laboratory test results are uploaded

---
+++ Laboratory_Work_1_Warming_Up_for_Computer_Design/DataPathTest/hdl/ALU.v
@@ -0,0 +1,134 @@
+module ALU #(parameter W=8)
+	(
+	input [W-1:0] inp_src0, 
+	input [W-1:0] inp_src1, 
+	input [3:0] operation_select, 
+	input [0:0] inp_carry,
+	output reg [W-1:0] out_alu,
+	output reg [0:0] carry_out_flag,
+	output reg [0:0] overflow_flag,
+	output reg [0:0] negative_flag,
+	output reg [0:0] zero_flag
+	);
+	
+	reg [W-1:0] inp_src0_1complement;
+	reg [W-1:0] inp_src1_1complement;
+	
+	reg [W-1:0] inp_src0_2complement;
+	reg [W-1:0] inp_src1_2complement;
+  
+  
+	function [0:0] out_alu_is_zero (input [W-1:0] alu_result);
+		out_alu_is_zero = (alu_result == 0);
+	endfunction
+
+
+	function [0:0] out_alu_is_negative (input [W-1:0] alu_result);
+		out_alu_is_negative = (alu_result[W-1] == 1);
+	endfunction
+	  
+	 
+	always @(*)
+	begin
+		inp_src0_1complement = ~inp_src0;
+		inp_src1_1complement = ~inp_src1;
+		
+		inp_src0_2complement = ~inp_src0 + 1;
+		inp_src1_2complement = ~inp_src1 + 1;
+		
+		case (operation_select)
+			4'd0: // AND
+				begin 
+					out_alu = inp_src0 & inp_src1;
+					carry_out_flag = 0;
+					overflow_flag = 0;
+					zero_flag = out_alu_is_zero(out_alu); 
+					negative_flag = out_alu_is_negative(out_alu);
+				end
+			4'd1: // XOR
+				begin 
+					out_alu = inp_src0 ^ inp_src1;
+					carry_out_flag = 0;
+					overflow_flag = 0;
+					zero_flag = out_alu_is_zero(out_alu); 
+					negative_flag = out_alu_is_negative(out_alu);
+				end
+			4'd2: // SUBSTRACTION A-B ? 
+				begin 
+					
+					{carry_out_flag, out_alu} = inp_src0 + inp_src1_2complement;
+					overflow_flag = ((inp_src1[W-1] != inp_src0[W-1]) && (inp_src0[W-1] != out_alu[W-1]));
+					zero_flag = out_alu_is_zero(out_alu); 
+					negative_flag = out_alu_is_negative(out_alu);
+				end
+			4'd3:  // SUBSTRACTION B-A ? 
+				begin 
+					{carry_out_flag, out_alu} = inp_src0_2complement + inp_src1;
+					overflow_flag = ((inp_src1[W-1] != inp_src0[W-1]) && (inp_src1[W-1] != out_alu[W-1]));
+					zero_flag = out_alu_is_zero(out_alu); 
+					negative_flag = out_alu_is_negative(out_alu);
+				end
+			4'd4: // ADDITION A+B
+				begin 
+					{carry_out_flag, out_alu}  = inp_src0 + inp_src1;
+					overflow_flag = ((inp_src1[W-1] == inp_src0[W-1]) && (inp_src0[W-1] != out_alu[W-1]));
+					zero_flag = out_alu_is_zero(out_alu); 
+					negative_flag = out_alu_is_negative(out_alu);
+				end
+			4'd5: // ADDITION A+B+CARRY
+				begin 
+					{carry_out_flag, out_alu}  = inp_src0 + inp_src1 + inp_carry;
+					overflow_flag = ((inp_src1[W-1] == inp_src0[W-1]) && (inp_src0[W-1] != out_alu[W-1]));
+					zero_flag = out_alu_is_zero(out_alu); 
+					negative_flag = out_alu_is_negative(out_alu);
+				end
+			4'd6: // SUBSTRACTION A-B+CARRY-1 ?
+				begin 
+					{carry_out_flag, out_alu}  = inp_src0 + inp_src1_2complement + inp_carry - 1;
+					overflow_flag = ((inp_src1[W-1] != inp_src0[W-1]) && (inp_src0[W-1] != out_alu[W-1]));
+					zero_flag = out_alu_is_zero(out_alu); 
+					negative_flag = out_alu_is_negative(out_alu);
+				end
+			4'd7: // SUBSTRACTION B-A+CARRY-1 ?
+				begin 
+					{carry_out_flag, out_alu}  = inp_src0_2complement + inp_src1 + inp_carry - 1;
+					overflow_flag = ((inp_src1[W-1] != inp_src0[W-1]) && (inp_src1[W-1] != out_alu[W-1]));
+					zero_flag = out_alu_is_zero(out_alu); 
+					negative_flag = out_alu_is_negative(out_alu);
+				end
+			4'd12: // ORR
+				begin 
+					out_alu = inp_src0 | inp_src1;
+					carry_out_flag = 0;
+					overflow_flag = 0;
+					zero_flag = out_alu_is_zero(out_alu); 
+					negative_flag = out_alu_is_negative(out_alu);
+				end
+			4'd13: // MOVE
+				begin 
+					out_alu = inp_src1;
+					carry_out_flag = 0;
+					overflow_flag = 0;
+					zero_flag = out_alu_is_zero(out_alu); 
+					negative_flag = out_alu_is_negative(out_alu);
+				end
+			4'd14: // BIT CLEAR
+				begin 
+					out_alu = inp_src0 & (~inp_src1);
+					carry_out_flag = 0;
+					overflow_flag = 0;
+					zero_flag = out_alu_is_zero(out_alu); 
+					negative_flag = out_alu_is_negative(out_alu);
+				end
+			4'd15: // MOVE NOT
+				begin 
+					out_alu = ~inp_src1; 
+					carry_out_flag = 0;
+					overflow_flag = 0;
+					zero_flag = out_alu_is_zero(out_alu); 
+					negative_flag = out_alu_is_negative(out_alu);
+				end
+		endcase
+	end
+
+endmodule
---
+++ SRC_Laboratory_Work_1_Warming_Up_for_Computer_Design/data_path_v.v
@@ -0,0 +1,122 @@
+// Copyright (C) 1991-2013 Altera Corporation
+// Your use of Altera Corporation's design tools, logic functions 
+// and other software and tools, and its AMPP partner logic 
+// functions, and any output files from any of the foregoing 
+// (including device programming or simulation files), and any 
+// associated documentation or information are expressly subject 
+// to the terms and conditions of the Altera Program License 
+// Subscription Agreement, Altera MegaCore Function License 
+// Agreement, or other applicable license agreement, including, 
+// without limitation, that your use is for the sole purpose of 
+// programming logic devices manufactured by Altera and sold by 
+// Altera or its authorized distributors.  Please refer to the 
+// applicable agreement for further details.
+
+// PROGRAM		"Quartus II 64-Bit"
+// VERSION		"Version 13.1.0 Build 162 10/23/2013 SJ Web Edition"
+// CREATED		"Tue Apr 11 17:37:02 2023"
+
+module data_path_v(
+	input_clk,
+	inp_alu_carry,
+	task_select
+);
+
+
+input wire	input_clk;
+input wire	inp_alu_carry;
+input wire	[3:0] task_select;
+
+wire	[1:0] SYNTHESIZED_WIRE_0;
+wire	[7:0] SYNTHESIZED_WIRE_18;
+wire	[4:0] SYNTHESIZED_WIRE_2;
+wire	SYNTHESIZED_WIRE_3;
+wire	SYNTHESIZED_WIRE_4;
+wire	[7:0] SYNTHESIZED_WIRE_5;
+wire	[7:0] SYNTHESIZED_WIRE_6;
+wire	[7:0] SYNTHESIZED_WIRE_7;
+wire	[0:0] SYNTHESIZED_WIRE_8;
+wire	[7:0] SYNTHESIZED_WIRE_10;
+wire	[0:0] SYNTHESIZED_WIRE_11;
+wire	[7:0] SYNTHESIZED_WIRE_12;
+wire	[7:0] SYNTHESIZED_WIRE_13;
+wire	[3:0] SYNTHESIZED_WIRE_14;
+wire	[7:0] SYNTHESIZED_WIRE_15;
+wire	[7:0] SYNTHESIZED_WIRE_16;
+wire	[0:0] SYNTHESIZED_WIRE_17;
+
+
+
+
+
+combinational_shifter	b2v_inst(
+	.control(SYNTHESIZED_WIRE_0),
+	.inp_shifter(SYNTHESIZED_WIRE_18),
+	.shamt(SYNTHESIZED_WIRE_2),
+	.out_shifter(SYNTHESIZED_WIRE_7));
+	defparam	b2v_inst.W = 8;
+
+
+register_synchronous_reset_write_en	b2v_inst1(
+	.clk(input_clk),
+	.write_enable(SYNTHESIZED_WIRE_3),
+	.reset_synchronous(SYNTHESIZED_WIRE_4),
+	.inp_reg(SYNTHESIZED_WIRE_5),
+	.out_reg(SYNTHESIZED_WIRE_18));
+	defparam	b2v_inst1.W = 8;
+
+
+multiplexer2to1	b2v_inst13(
+	.inp_mux0(SYNTHESIZED_WIRE_6),
+	.inp_mux1(SYNTHESIZED_WIRE_7),
+	.select(SYNTHESIZED_WIRE_8),
+	.out_mux(SYNTHESIZED_WIRE_15));
+	defparam	b2v_inst13.W = 8;
+
+
+controller	b2v_inst16(
+	.task_select(task_select),
+	.write_enable(SYNTHESIZED_WIRE_3),
+	.reset_synchronous(SYNTHESIZED_WIRE_4),
+	.ALU_OPERATION_SELECT(SYNTHESIZED_WIRE_14),
+	.combinational_shifter_control(SYNTHESIZED_WIRE_0),
+	.comp2_mux2_inp_1(SYNTHESIZED_WIRE_6),
+	.MUX0_INP1(SYNTHESIZED_WIRE_10),
+	.MUX0_SELECT(SYNTHESIZED_WIRE_11),
+	.MUX1_INP1(SYNTHESIZED_WIRE_16),
+	.MUX1_SELECT(SYNTHESIZED_WIRE_17),
+	.op2_mux2_select(SYNTHESIZED_WIRE_8),
+	.shamt(SYNTHESIZED_WIRE_2));
+	defparam	b2v_inst16.W = 8;
+
+
+multiplexer2to1	b2v_inst2(
+	.inp_mux0(SYNTHESIZED_WIRE_18),
+	.inp_mux1(SYNTHESIZED_WIRE_10),
+	.select(SYNTHESIZED_WIRE_11),
+	.out_mux(SYNTHESIZED_WIRE_12));
+	defparam	b2v_inst2.W = 8;
+
+
+ALU	b2v_inst3(
+	.inp_carry(inp_alu_carry),
+	.inp_src0(SYNTHESIZED_WIRE_12),
+	.inp_src1(SYNTHESIZED_WIRE_13),
+	.operation_select(SYNTHESIZED_WIRE_14),
+	
+	
+	.out_alu(SYNTHESIZED_WIRE_5)
+	
+	);
+	defparam	b2v_inst3.W = 8;
+
+
+multiplexer2to1	b2v_inst4(
+	.inp_mux0(SYNTHESIZED_WIRE_15),
+	.inp_mux1(SYNTHESIZED_WIRE_16),
+	.select(SYNTHESIZED_WIRE_17),
+	.out_mux(SYNTHESIZED_WIRE_13));
+	defparam	b2v_inst4.W = 8;
+
+
+endmodule
