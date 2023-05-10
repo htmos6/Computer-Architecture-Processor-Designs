7ea8e0f33d8717747f2a2126c04acca63aef9bda
htmos6 <mehmet.arslan4000@gmail.com>
2023-05-10 12:22:03 +0300

    multi cycle controller and overall data path verilog files

--- SRC_Laboratory_Work_3_Multi_Cycle_Processor_Design/controller.v
+++ SRC_Laboratory_Work_3_Multi_Cycle_Processor_Design/controller.v
@@ -38,8 +38,8 @@ module controller #(parameter W=32)
     wire [1:0] NZ_flags_reg_out;
     wire [1:0] CV_flags_reg_out;
 
-    reg [0:0] CondEx;
-    // assign CondEx = ((Cond == 4'b1110) || (Cond == 4'b0000 && NZ_flags_reg_out[0] == 1'b1) || (Cond == 4'b0001 &&  NZ_flags_reg_out[0] == 1'b0));
+    wire [0:0] CondEx;
+    assign CondEx = ((Cond == 4'b1110) || (Cond == 4'b0000 && NZ_flags_reg_out[0] == 1'b1) || (Cond == 4'b0001 &&  NZ_flags_reg_out[0] == 1'b0));
     // assign CondEx = 1;
 
     register_synchronous_reset_write_en #(.W(2)) NZ_Flags_Reg (.clk(clk), .write_enable(write_enable_NZ), .reset_synchronous(0), .inp_reg( {negative_flag, zero_flag} ), .out_reg(NZ_flags_reg_out));
@@ -47,8 +47,8 @@ module controller #(parameter W=32)
 
     initial 
         begin
-            state_number = 0;
-            CondEx = 1;
+            state_number = 11;
+            //CondEx = 1;
         end
     
 
@@ -133,6 +133,10 @@ module controller #(parameter W=32)
                         // BRANCH SIGNAL
                     end
                 4'd10: // BX
+                    begin
+                        state_number <= 0;
+                    end
+                4'd11: // BX
                     begin
                         state_number <= 0;
                     end
--- SRC_Laboratory_Work_3_Multi_Cycle_Processor_Design/data_path_3.v
+++ SRC_Laboratory_Work_3_Multi_Cycle_Processor_Design/data_path_3.v
@@ -14,11 +14,12 @@
 
 // PROGRAM		"Quartus II 64-Bit"
 // VERSION		"Version 13.1.0 Build 162 10/23/2013 SJ Web Edition"
-// CREATED		"Mon May 08 10:25:04 2023"
+// CREATED		"Wed May 10 01:44:53 2023"
 
 module data_path_3(
 	clk,
 	reg_reset,
+	reset_controller,
 	alu_out,
 	rd0,
 	rd1
@@ -27,6 +28,7 @@ module data_path_3(
 
 input wire	clk;
 input wire	reg_reset;
+input wire	reset_controller;
 output wire	[31:0] alu_out;
 output wire	[31:0] rd0;
 output wire	[31:0] rd1;
@@ -36,18 +38,18 @@ wire	[1:0] reg_src;
 wire	SYNTHESIZED_WIRE_0;
 wire	[3:0] SYNTHESIZED_WIRE_1;
 wire	[3:0] SYNTHESIZED_WIRE_2;
-wire	[31:0] SYNTHESIZED_WIRE_50;
-wire	[31:0] SYNTHESIZED_WIRE_5;
+wire	[31:0] SYNTHESIZED_WIRE_48;
+wire	[31:0] SYNTHESIZED_WIRE_49;
 wire	[31:0] SYNTHESIZED_WIRE_6;
 wire	[31:0] SYNTHESIZED_WIRE_7;
 wire	[1:0] SYNTHESIZED_WIRE_8;
 wire	[31:0] SYNTHESIZED_WIRE_9;
 wire	[31:0] SYNTHESIZED_WIRE_10;
-wire	[31:0] SYNTHESIZED_WIRE_51;
+wire	[31:0] SYNTHESIZED_WIRE_50;
 wire	[1:0] SYNTHESIZED_WIRE_12;
 wire	SYNTHESIZED_WIRE_13;
 wire	SYNTHESIZED_WIRE_14;
-wire	[31:0] SYNTHESIZED_WIRE_52;
+wire	[31:0] SYNTHESIZED_WIRE_51;
 wire	SYNTHESIZED_WIRE_16;
 wire	SYNTHESIZED_WIRE_18;
 wire	SYNTHESIZED_WIRE_20;
@@ -61,23 +63,21 @@ wire	SYNTHESIZED_WIRE_27;
 wire	SYNTHESIZED_WIRE_29;
 wire	SYNTHESIZED_WIRE_30;
 wire	[31:0] SYNTHESIZED_WIRE_31;
-wire	[31:0] SYNTHESIZED_WIRE_53;
-wire	[31:0] SYNTHESIZED_WIRE_33;
-wire	[0:0] SYNTHESIZED_WIRE_35;
-wire	[31:0] SYNTHESIZED_WIRE_36;
-wire	[31:0] SYNTHESIZED_WIRE_37;
-wire	[3:0] SYNTHESIZED_WIRE_38;
+wire	[0:0] SYNTHESIZED_WIRE_33;
+wire	[31:0] SYNTHESIZED_WIRE_34;
+wire	[31:0] SYNTHESIZED_WIRE_35;
+wire	[3:0] SYNTHESIZED_WIRE_36;
+wire	[31:0] SYNTHESIZED_WIRE_52;
 wire	SYNTHESIZED_WIRE_39;
 wire	SYNTHESIZED_WIRE_40;
 wire	SYNTHESIZED_WIRE_41;
 wire	SYNTHESIZED_WIRE_42;
-wire	[31:0] SYNTHESIZED_WIRE_54;
-wire	SYNTHESIZED_WIRE_45;
-wire	[3:0] SYNTHESIZED_WIRE_46;
-wire	[31:0] SYNTHESIZED_WIRE_47;
-wire	SYNTHESIZED_WIRE_49;
+wire	SYNTHESIZED_WIRE_43;
+wire	[3:0] SYNTHESIZED_WIRE_44;
+wire	[31:0] SYNTHESIZED_WIRE_45;
+wire	SYNTHESIZED_WIRE_47;
 
-assign	alu_out = SYNTHESIZED_WIRE_51;
+assign	alu_out = SYNTHESIZED_WIRE_50;
 assign	rd0 = SYNTHESIZED_WIRE_21;
 assign	rd1 = SYNTHESIZED_WIRE_23;
 assign	SYNTHESIZED_WIRE_14 = 0;
@@ -99,30 +99,30 @@ register_file	b2v_inst1(
 	.inp_read_address0(SYNTHESIZED_WIRE_1),
 	.inp_read_address1(SYNTHESIZED_WIRE_2),
 	.inp_write_address0(inst[15:12]),
-	.inp_write_data(SYNTHESIZED_WIRE_50),
-	.inp_write_data_R15(SYNTHESIZED_WIRE_50),
+	.inp_write_data(SYNTHESIZED_WIRE_48),
+	.inp_write_data_R15(SYNTHESIZED_WIRE_48),
 	.out_read_data0(SYNTHESIZED_WIRE_21),
 	.out_read_data1(SYNTHESIZED_WIRE_23));
 	defparam	b2v_inst1.W = 32;
 
 
 multiplexer4to1	b2v_inst10(
-	.inp_mux0(SYNTHESIZED_WIRE_5),
+	.inp_mux0(SYNTHESIZED_WIRE_49),
 	.inp_mux1(SYNTHESIZED_WIRE_6),
 	.inp_mux2(SYNTHESIZED_WIRE_7),
 	
 	.select(SYNTHESIZED_WIRE_8),
-	.out_mux(SYNTHESIZED_WIRE_37));
+	.out_mux(SYNTHESIZED_WIRE_35));
 	defparam	b2v_inst10.W = 32;
 
 
 multiplexer4to1	b2v_inst11(
 	.inp_mux0(SYNTHESIZED_WIRE_9),
 	.inp_mux1(SYNTHESIZED_WIRE_10),
-	.inp_mux2(SYNTHESIZED_WIRE_51),
+	.inp_mux2(SYNTHESIZED_WIRE_50),
 	
 	.select(SYNTHESIZED_WIRE_12),
-	.out_mux(SYNTHESIZED_WIRE_50));
+	.out_mux(SYNTHESIZED_WIRE_48));
 	defparam	b2v_inst11.W = 32;
 
 
@@ -130,7 +130,7 @@ register_synchronous_reset_write_en	b2v_inst12(
 	.clk(clk),
 	.write_enable(SYNTHESIZED_WIRE_13),
 	.reset_synchronous(SYNTHESIZED_WIRE_14),
-	.inp_reg(SYNTHESIZED_WIRE_52),
+	.inp_reg(SYNTHESIZED_WIRE_51),
 	.out_reg(inst));
 	defparam	b2v_inst12.W = 32;
 
@@ -138,7 +138,7 @@ register_synchronous_reset_write_en	b2v_inst12(
 register_synchronous_reset	b2v_inst15(
 	.clk(clk),
 	.reset_synchronous(SYNTHESIZED_WIRE_16),
-	.inp_reg(SYNTHESIZED_WIRE_52),
+	.inp_reg(SYNTHESIZED_WIRE_51),
 	.out_reg(SYNTHESIZED_WIRE_10));
 	defparam	b2v_inst15.W = 32;
 
@@ -146,7 +146,7 @@ register_synchronous_reset	b2v_inst15(
 register_synchronous_reset	b2v_inst16(
 	.clk(clk),
 	.reset_synchronous(SYNTHESIZED_WIRE_18),
-	.inp_reg(SYNTHESIZED_WIRE_51),
+	.inp_reg(SYNTHESIZED_WIRE_50),
 	.out_reg(SYNTHESIZED_WIRE_9));
 	defparam	b2v_inst16.W = 32;
 
@@ -155,7 +155,7 @@ register_synchronous_reset	b2v_inst17(
 	.clk(clk),
 	.reset_synchronous(SYNTHESIZED_WIRE_20),
 	.inp_reg(SYNTHESIZED_WIRE_21),
-	.out_reg(SYNTHESIZED_WIRE_47));
+	.out_reg(SYNTHESIZED_WIRE_45));
 	defparam	b2v_inst17.W = 32;
 
 
@@ -163,7 +163,7 @@ register_synchronous_reset	b2v_inst18(
 	.clk(clk),
 	.reset_synchronous(SYNTHESIZED_WIRE_22),
 	.inp_reg(SYNTHESIZED_WIRE_23),
-	.out_reg(SYNTHESIZED_WIRE_53));
+	.out_reg(SYNTHESIZED_WIRE_49));
 	defparam	b2v_inst18.W = 32;
 
 
@@ -177,7 +177,7 @@ constant_value_generator	b2v_inst19(
 extend_immediate	b2v_inst2(
 	.ImmSrc(SYNTHESIZED_WIRE_25),
 	.instruction(inst),
-	.out_ext_imm(SYNTHESIZED_WIRE_33));
+	.out_ext_imm(SYNTHESIZED_WIRE_6));
 	defparam	b2v_inst2.W = 32;
 
 
@@ -185,14 +185,14 @@ register_synchronous_reset_write_en	b2v_inst21(
 	.clk(clk),
 	.write_enable(SYNTHESIZED_WIRE_26),
 	.reset_synchronous(SYNTHESIZED_WIRE_27),
-	.inp_reg(SYNTHESIZED_WIRE_50),
-	.out_reg(SYNTHESIZED_WIRE_54));
+	.inp_reg(SYNTHESIZED_WIRE_48),
+	.out_reg(SYNTHESIZED_WIRE_52));
 	defparam	b2v_inst21.W = 32;
 
 
 constant_value_generator	b2v_inst22(
 	.reset_synchronous(SYNTHESIZED_WIRE_29),
-	.out_reg(SYNTHESIZED_WIRE_46));
+	.out_reg(SYNTHESIZED_WIRE_44));
 	defparam	b2v_inst22.value = 15;
 	defparam	b2v_inst22.W = 4;
 
@@ -207,49 +207,42 @@ memory	b2v_inst3(
 	.clk(clk),
 	.WE(SYNTHESIZED_WIRE_30),
 	.ADDR(SYNTHESIZED_WIRE_31),
-	.WD(SYNTHESIZED_WIRE_53),
-	.RD(SYNTHESIZED_WIRE_52));
+	.WD(SYNTHESIZED_WIRE_49),
+	.RD(SYNTHESIZED_WIRE_51));
 	defparam	b2v_inst3.ADDR_WIDTH = 32;
 	defparam	b2v_inst3.BYTE_SIZE = 4;
 
 
 
 
-combinational_shifter	b2v_inst33(
-	.control(inst[6:5]),
-	.inp_shifter(SYNTHESIZED_WIRE_33),
-	.shamt(inst[11:7]),
-	.out_shifter(SYNTHESIZED_WIRE_6));
-	defparam	b2v_inst33.W = 32;
-
-
-combinational_shifter	b2v_inst34(
-	.control(inst[6:5]),
-	.inp_shifter(SYNTHESIZED_WIRE_53),
-	.shamt(inst[11:7]),
-	.out_shifter(SYNTHESIZED_WIRE_5));
-	defparam	b2v_inst34.W = 32;
-
-
 ALU	b2v_inst4(
-	.inp_carry(SYNTHESIZED_WIRE_35),
-	.inp_src0(SYNTHESIZED_WIRE_36),
-	.inp_src1(SYNTHESIZED_WIRE_37),
-	.operation_select(SYNTHESIZED_WIRE_38),
-	.carry_out_flag(SYNTHESIZED_WIRE_39),
-	.overflow_flag(SYNTHESIZED_WIRE_40),
-	.negative_flag(SYNTHESIZED_WIRE_41),
-	.zero_flag(SYNTHESIZED_WIRE_42),
-	.out_alu(SYNTHESIZED_WIRE_51));
+	.inp_carry(SYNTHESIZED_WIRE_33),
+	.inp_src0(SYNTHESIZED_WIRE_34),
+	.inp_src1(SYNTHESIZED_WIRE_35),
+	.operation_select(SYNTHESIZED_WIRE_36),
+	.carry_out_flag(SYNTHESIZED_WIRE_40),
+	.overflow_flag(SYNTHESIZED_WIRE_41),
+	.negative_flag(SYNTHESIZED_WIRE_42),
+	.zero_flag(SYNTHESIZED_WIRE_43),
+	.out_alu(SYNTHESIZED_WIRE_50));
 	defparam	b2v_inst4.WIDTH = 32;
 
 
-controller	b2v_inst56(
+multiplexer2to1	b2v_inst6(
+	.inp_mux0(SYNTHESIZED_WIRE_52),
+	.inp_mux1(SYNTHESIZED_WIRE_48),
+	.select(SYNTHESIZED_WIRE_39),
+	.out_mux(SYNTHESIZED_WIRE_31));
+	defparam	b2v_inst6.W = 32;
+
+
+controller	b2v_inst66(
 	.clk(clk),
-	.carry_out_flag(SYNTHESIZED_WIRE_39),
-	.overflow_flag(SYNTHESIZED_WIRE_40),
-	.negative_flag(SYNTHESIZED_WIRE_41),
-	.zero_flag(SYNTHESIZED_WIRE_42),
+	.carry_out_flag(SYNTHESIZED_WIRE_40),
+	.overflow_flag(SYNTHESIZED_WIRE_41),
+	.negative_flag(SYNTHESIZED_WIRE_42),
+	.zero_flag(SYNTHESIZED_WIRE_43),
+	.reset(reset_controller),
 	.Cond(inst[31:28]),
 	.Func(inst[25:20]),
 	.inst_19_to_4_BX(inst[19:4]),
@@ -259,28 +252,20 @@ controller	b2v_inst56(
 	.RegWrite(SYNTHESIZED_WIRE_0),
 	.MemWrite(SYNTHESIZED_WIRE_30),
 	.IRWrite(SYNTHESIZED_WIRE_13),
-	.AdrSrc(SYNTHESIZED_WIRE_45),
-	.AluSrcA(SYNTHESIZED_WIRE_49),
-	.AluControl(SYNTHESIZED_WIRE_38),
+	.AdrSrc(SYNTHESIZED_WIRE_39),
+	.AluSrcA(SYNTHESIZED_WIRE_47),
+	.AluControl(SYNTHESIZED_WIRE_36),
 	.AluSrcB(SYNTHESIZED_WIRE_8),
-	.C_flag_reg_out(SYNTHESIZED_WIRE_35),
+	.C_flag_reg_out(SYNTHESIZED_WIRE_33),
 	.ImmSrc(SYNTHESIZED_WIRE_25),
 	.RegSrc(reg_src),
 	.ResultSrc(SYNTHESIZED_WIRE_12));
-	defparam	b2v_inst56.W = 32;
-
-
-multiplexer2to1	b2v_inst6(
-	.inp_mux0(SYNTHESIZED_WIRE_54),
-	.inp_mux1(SYNTHESIZED_WIRE_50),
-	.select(SYNTHESIZED_WIRE_45),
-	.out_mux(SYNTHESIZED_WIRE_31));
-	defparam	b2v_inst6.W = 32;
+	defparam	b2v_inst66.W = 32;
 
 
 multiplexer2to1	b2v_inst7(
 	.inp_mux0(inst[19:16]),
-	.inp_mux1(SYNTHESIZED_WIRE_46),
+	.inp_mux1(SYNTHESIZED_WIRE_44),
 	.select(reg_src[0]),
 	.out_mux(SYNTHESIZED_WIRE_1));
 	defparam	b2v_inst7.W = 4;
@@ -295,10 +280,10 @@ multiplexer2to1	b2v_inst8(
 
 
 multiplexer2to1	b2v_inst9(
-	.inp_mux0(SYNTHESIZED_WIRE_47),
-	.inp_mux1(SYNTHESIZED_WIRE_54),
-	.select(SYNTHESIZED_WIRE_49),
-	.out_mux(SYNTHESIZED_WIRE_36));
+	.inp_mux0(SYNTHESIZED_WIRE_45),
+	.inp_mux1(SYNTHESIZED_WIRE_52),
+	.select(SYNTHESIZED_WIRE_47),
+	.out_mux(SYNTHESIZED_WIRE_34));
 	defparam	b2v_inst9.W = 32;
 
 
